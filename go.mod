d1bc458d19ad4f58b77cf94402bbec87c0b07a35
sarox0987 <sarox0987@gmail.com>
2024-02-04 00:45:03 +0330

    update

---
+++ go.mod
@@ -0,0 +1,39 @@
+module test
+
+go 1.21.6
+
+require (
+	github.com/NethermindEth/juno v0.9.4
+	github.com/NethermindEth/starknet.go v0.6.1
+	github.com/ethereum/go-ethereum v1.13.11
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/test-go/testify v1.1.4 // indirect
+	github.com/tklauser/go-sysconf v0.3.13 // indirect
+	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/starknet.go/account"
+	"github.com/NethermindEth/starknet.go/curve"
+	"github.com/NethermindEth/starknet.go/rpc"
+	ethrpc "github.com/ethereum/go-ethereum/rpc"
+
+	"github.com/NethermindEth/starknet.go/utils"
+)
+
+var (
+	name                  string = "testnet"
+	account_addr          string = "0x0000000000000000000000000000000000000000000000000000000000000004"
+	account_cairo_version        = 0
+	predeployedClassHash         = "0x2794ce20e5f2ff0d40e632cb53845b9f4e526ebd8471983f7dbd355b721d5a"
+	privateKey            string = "0x00c0cf1490de1352865301bb8705143f3ef938f96fdf892f1091dcb5ac7bcd1d"
+	ethContract           string = "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+	contractMethod        string = "transfer"
+)
+
+type RPC struct {
+	Url string
+}
+
+func main() {
+	file, err := os.Open("rpc.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	r := &RPC{}
+	if err := json.Unmarshal(data, r); err != nil {
+		panic(err)
+	}
+
+	c, err := ethrpc.DialContext(context.Background(), r.Url)
+	if err != nil {
+		panic(err)
+	}
+	p := rpc.NewProvider(c)
+
+	account_address, err := utils.HexToFelt(account_addr)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	public_key := pubKey(privateKey)
+
+	ks := account.NewMemKeystore()
+	fakePrivKeyBI, ok := new(big.Int).SetString(privateKey, 0)
+	if !ok {
+		panic(err.Error())
+	}
+	ks.Put(public_key, fakePrivKeyBI)
+
+	a, err := account.NewAccount(p, account_address, public_key, ks, 2)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	go deployTx(public_key, a)
+	go invokeTx(a)
+	select {}
+
+}
+
+func deployTx(pub string, a *account.Account) {
+	classHash, err := utils.HexToFelt(predeployedClassHash)
+	if err != nil {
+		panic(err)
+	}
+
+	pubKey, _ := utils.HexToFelt(pub)
+
+	tx := rpc.DeployAccountTxn{
+		Nonce:               &felt.Zero,
+		MaxFee:              new(felt.Felt).SetUint64(4724395326064),
+		Type:                rpc.TransactionType_DeployAccount,
+		Version:             rpc.TransactionV1,
+		Signature:           []*felt.Felt{},
+		ClassHash:           classHash,
+		ContractAddressSalt: pubKey,
+		ConstructorCalldata: []*felt.Felt{pubKey},
+	}
+
+	precomputedAddress, _ := a.PrecomputeAddress(&felt.Zero, pubKey, classHash, tx.ConstructorCalldata)
+
+	err = a.SignDeployAccountTransaction(context.Background(), &tx, precomputedAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+
+		resp, err := a.AddDeployAccountTransaction(context.Background(), rpc.BroadcastDeployAccountTxn{DeployAccountTxn: tx})
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(41 * time.Second)
+			continue
+		}
+		fmt.Println("deployd: ", resp.ContractAddress)
+		time.Sleep(21 * time.Second)
+
+	}
+
+}
+
+func invokeTx(a *account.Account) {
+	for {
+		nonce, err := a.Nonce(context.Background(), rpc.BlockID{Tag: "latest"}, a.AccountAddress)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		ca, err := utils.HexToFelt(ethContract)
+		if err != nil {
+			panic(err.Error())
+		}
+		recipient, err := utils.HexToFelt("0x054649B7bF9e490E7098265895af70E6fB7DD7e6610E605f9eC27C8afE8b343b")
+		if err != nil {
+			panic(err.Error())
+			continue
+		}
+
+		amount := utils.BigIntToFelt(big.NewInt(1))
+		maxfee := utils.BigIntToFelt(big.NewInt(10000))
+
+		InvokeTx := rpc.InvokeTxnV1{
+			MaxFee:        maxfee,
+			Version:       rpc.TransactionV1,
+			Nonce:         nonce,
+			Type:          rpc.TransactionType_Invoke,
+			SenderAddress: a.AccountAddress,
+		}
+
+		FnCall := rpc.FunctionCall{
+			ContractAddress:    ca,
+			EntryPointSelector: utils.GetSelectorFromNameFelt(contractMethod),
+			Calldata:           []*felt.Felt{recipient, amount},
+		}
+
+		InvokeTx.Calldata, err = a.FmtCalldata([]rpc.FunctionCall{FnCall})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		err = a.SignInvokeTransaction(context.Background(), &InvokeTx)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		rsp, err := a.AddInvokeTransaction(context.Background(), InvokeTx)
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(40 * time.Second)
+			continue
+		}
+
+		fmt.Println("tx: ", rsp.TransactionHash, " nonce: ", nonce)
+		time.Sleep(20 * time.Second)
+	}
+}
+
+func pubKey(prvKey string) string {
+	pk, err := utils.HexToFelt(prvKey)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pubKey, _, err := curve.Curve.PrivateToPoint(utils.FeltToBigInt(pk))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return pubKey.String()
+}
